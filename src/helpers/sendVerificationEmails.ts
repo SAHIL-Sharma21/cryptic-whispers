79a09a0b98dfab33bee666f6e775844f67af4ff8
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-05-22 14:02:27 +0530

    logic for verification email completed

--- src/helpers/sendVerificationEmails.ts
+++ src/helpers/sendVerificationEmails.ts
@@ -3,3 +3,35 @@
 import {resend} from '@/lib/resend'
 
 import VerificationEmailTemplate from '../../emails/VerificationEmailTemplate'
+
+import {ApiResponse} from '@/types/ApiResponse'
+
+//now email method: emails are always async
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+): Promise<ApiResponse> {
+    try {
+        //sending email logic read from documentation.
+        await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: email,
+            subject: "Cryptic whispers | Verification Code",
+            react: VerificationEmailTemplate({username, otp: verifyCode}),
+        });
+
+        return {
+            success: true,
+            message: "Verification email send successfully!"
+        }
+    } catch (emailError) {
+        console.error("Error sending verification email.", emailError);
+        //returing so that ts did not give warning
+        return {
+            success: false,
+            message: "Failed to send verification email."
+        }
+    }
+}

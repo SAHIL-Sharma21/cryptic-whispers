583867e6621edb5d8ddfe9296c1df12edc271cf3
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-05-28 23:14:04 +0530

    implementing logic with backend and form progesss

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -11,6 +11,8 @@ import { useRouter } from "next/navigation"
 import { signUpValidation } from "@/schemas/signUpSchema"
 import axios, {AxiosError} from 'axios';
 import { ApiResponse } from "@/types/ApiResponse"
+import { Form, FormField, FormItem, FormLabel, FormControl, FormDescription, FormMessage } from "@/components/ui/form"
+import {Input} from "@/components/ui/input"
 
 const SignIn = () => {
 
@@ -60,13 +62,72 @@ const SignIn = () => {
 
     //calling the function
     checkUsernameUnique();
-
-
   }, [debouncedUsername]);
 
+  //making one method for submitting the form
+  const onSubmit = async(data: z.infer<typeof signUpValidation>) => {
+    setIsSubmitting(true);
+    try {
+     const response = await axios.post<ApiResponse>(`/api/sign-up`, data);
+     console.log(response.data);
+     //if we get the response then user ko toast messaeg show kr denge
+     toast({
+      title: 'Success',
+      description: response.data.message,
+     });
+     //router use krke user ko new route pr le jayenge 
+     router.replace(`/verify/${username}`)
+     setIsSubmitting(false);
+    } catch (error) {
+      console.error("error in signup of user", error);
+      const axiosError = error as AxiosError<ApiResponse>;
+      let errorMessage = axiosError.response?.data.message;
+      toast({
+        title: "Signup failed",
+        description: errorMessage,
+        variant: "destructive"
+      });
+      setIsSubmitting(false);
+    }
+  }
 
   return (
-    <div>page</div>
+    <>
+      <div className="flex justify-center items-center min-h-screen bg-gray-800">
+          <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+            <div className="text-center">
+                <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+                  Join Cryptic Whispers
+                </h1>
+                <p className="mb-4">Sign up to start your anonymous adventure</p>
+            </div>
+            
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <FormField
+              name="username"
+              control={form.control}
+              render={({ field }) => (
+              <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="username" 
+                    {...field} 
+                    onChange={(e) => {
+                      field.onChange(e);
+                      setUsername(e.target.value);
+                    }}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+                )}
+              />
+              </form>
+            </Form>
+          </div>
+      </div>
+    </>
   )
 }
 
